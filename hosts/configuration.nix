1bf1b856d4e1de8531eb1f043c43c1d8392e136b
rusha <hrishikesh.sawant322@gmail.com>
2025-08-23 23:34:55 +0900

    add user to trusted

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -50,6 +50,7 @@
 		settings = {
 			auto-optimise-store = true;
 			experimental-features = ["nix-command" "flakes"];
+            trusted-users = [ "root" "${userSettings.username}" ];
 		};
 		gc = {
 			automatic = true;
