90c8d511693a9248e646de5adfd8d64de0c8627f
rusha <hrishikesh.sawant322@gmail.com>
2026-01-29 21:47:00 +0900

    add github pat to sops

--- modules/security/sops.nix
+++ modules/security/sops.nix
@@ -13,5 +13,15 @@ in
         age.keyFile = keyFile;
 
         secrets."gemini_cli/api_key" = {};
+        secrets."nix/github_pat" = {
+            owner = "root";
+            group = "root";
+            mode = "0440";
+        };
     };
+
+    # Configure nix to use GitHub PAT from sops
+    nix.extraOptions = ''
+        access-tokens = github.com=${config.sops.secrets."nix/github_pat".path}
+    '';
 }
