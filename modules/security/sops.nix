e68d8849bc7c5a18f0122d4497f29df44f7ed04f
rusha <hrishikesh.sawant322@gmail.com>
2026-01-30 18:27:34 +0900

    use netrc file for github pat

--- modules/security/sops.nix
+++ modules/security/sops.nix
@@ -17,10 +17,35 @@ in
             group = "nixbld";
             mode = "0440";
         };
+        secrets."nix/github_username" = {
+            group = "nixbld";
+            mode = "0440";
+        };
     };
 
-    # Configure nix to use GitHub PAT from sops
-    nix.extraOptions = ''
-        access-tokens = github.com=${config.sops.secrets."nix/github_pat".path}
-    '';
+    # Configure nix to use netrc file
+    nix.settings.netrc-file = "/run/secrets/netrc";
+    
+    # Create systemd service to generate netrc from sops secrets
+    systemd.services.generate-netrc = {
+        description = "Generate netrc file from sops secrets";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "sops-nix.service" ];
+        serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+            User = "root";
+            Group = "root";
+        };
+        script = ''
+            mkdir -p /run/secrets
+            cat > /run/secrets/netrc << EOF
+machine github.com
+login $(cat ${config.sops.secrets."nix/github_username".path})
+password $(cat ${config.sops.secrets."nix/github_pat".path})
+EOF
+            chmod 644 /run/secrets/netrc
+            chown root:nixbld /run/secrets/netrc
+        '';
+    };
 }
